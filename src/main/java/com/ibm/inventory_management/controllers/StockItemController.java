4a2185a4d9b41fcdb5ab49037259d780d501a230
Dillon Carns <Dillon.Carns@ibm.com>
2020-01-10 13:06:12 -0500

    Adds StockItem service implementation

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -1,7 +1,9 @@
 package com.ibm.inventory_management.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import com.ibm.inventory_management.models.StockItem;
+import com.ibm.inventory_management.services.StockItemApi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StockItemController {
 
+    private final StockItemApi service;
+    
+    public StockItemController(StockItemApi service) {
+        this.service = service;
+    }
+
     @GetMapping(path = "/stock-items", produces = "application/json")
-    public List listStockItems() {
-        return new ArrayList();
+    public List<StockItem> listStockItems() {
+        return this.service.listStockItems();
     }
 }
