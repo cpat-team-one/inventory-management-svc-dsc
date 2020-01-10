0eb59243032fcdc36aef23eadedcee87a36a1238
Dillon Carns <Dillon.Carns@ibm.com>
2020-01-10 14:06:41 -0500

    Moved test to Test dir

--- src/test/java/com/ibm/inventory_management/controllers/StockItemControllerTest.java
+++ src/test/java/com/ibm/inventory_management/controllers/StockItemControllerTest.java
@@ -12,14 +12,14 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import com.ibm.inventory_management.models.StockItem;
 import com.ibm.inventory_management.services.StockItemApi;
 
-@DisplayName("StockItemController")
+@org.junit.jupiter.api.DisplayName("StockItemController")
 public class StockItemControllerTest {
     StockItemController controller;
     StockItemApi service;
@@ -31,22 +31,22 @@ public class StockItemControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
     @Nested
-    @DisplayName("Given [GET] /stock-items")
+    @org.junit.jupiter.api.DisplayName("Given [GET] /stock-items")
     public class GivenGetStockItems {
         @Test
-        @DisplayName("When called then it should return a 200 status")
+        @org.junit.jupiter.api.DisplayName("When called then it should return a 200 status")
         public void when_called_should_return_200_status() throws Exception {
             mockMvc.perform(get("/stock-items"))
                     .andExpect(status().isOk());
         }
         @Test
-        @DisplayName("When called then it should return an empty array")
+        @org.junit.jupiter.api.DisplayName("When called then it should return an empty array")
         public void when_called_then_return_an_empty_array() throws Exception {
             mockMvc.perform(get("/stock-items").accept("application/json"))
                     .andExpect(content().json("[]"));
         }
         @Test
-        @DisplayName("When called then it should return the results of the StockItemService")
+        @org.junit.jupiter.api.DisplayName("When called then it should return the results of the StockItemService")
         public void when_called_then_return_the_results_of_the_stockitemservice() throws Exception {
             final List<StockItem> expectedResult = Arrays.asList(new StockItem());
             when(service.listStockItems()).thenReturn(expectedResult);
